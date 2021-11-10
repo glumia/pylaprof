65126a5058a6ada69db8da6ce57d0e4f52b2217d
Giuseppe Lumia <g.lumia@outlook.com>
2021-11-10 09:17:16 +0100

    Decouple profiler from sampler
    oh, yes, and add the possibility to use the profiler as a decorator :)

--- pylaprof/__init__.py
+++ pylaprof/__init__.py
@@ -1,10 +1,40 @@
 import sys
 import threading
 from collections import defaultdict
+from functools import partial, wraps
+
+
+class Sampler:
+    def sample(self, frame):
+        pass
+
+    def dump(self):
+        pass
+
+
+class StackCollapse(Sampler):
+    def __init__(self):
+        super().__init__()
+        self._data = defaultdict(lambda: 0)
+
+    def sample(self, frame):
+        stack = []
+        while frame:
+            filename = frame.f_code.co_filename
+            funcname = frame.f_code.co_name
+            lineno = frame.f_lineno
+            stack.append(f"{funcname} ({filename}:{lineno})")
+            frame = frame.f_back
+        self._data[tuple(stack)] += 1
+
+    def dump(self):
+        with open("stackcollapse.out", "w") as fp:
+            for stack, hits in self._data.items():
+                print(";".join(stack[::-1]), hits, file=fp)
 
 
 class Profile(threading.Thread):
-    def __init__(self, period=0.01, single=True):
+    def __init__(self, period=0.01, single=True, sampler=None):
         """
         period (float)
           How many seconds to wait between consecutive samples.
@@ -14,9 +44,13 @@ class Profile(threading.Thread):
           session to get meaningful results.
         single (bool)
           Profile only the thread which created this instance.
+        sampler (Sampler)
+          Sampler to use to process stack frames.
+          Defaults to an instance of `StackCollapse` if none.
         """
-        self._data = defaultdict(lambda: 0)
-        self._test = None
+        if sampler is None:
+            self.sampler = StackCollapse()
+        self._test = lambda x, ident=threading.current_thread().ident: ident != x
         if single:
             self._test = lambda x, ident=threading.current_thread().ident: ident == x
 
@@ -50,27 +84,32 @@ class Profile(threading.Thread):
 
     def run(self):
         test = self._test
-        if test is None:
-            test = lambda x, ident=threading.current_thread().ident: ident != x
         stop_event = self._stop_event
+        wait = partial(stop_event.wait, self._period)
+        current_frames = sys._current_frames
+        sample = self.sampler.sample
         while self._can_run:
-            for ident, frame in sys._current_frames().items():
+            for ident, frame in current_frames().items():
                 if test(ident):
-                    stack = []
-                    while frame:
-                        filename = frame.f_code.co_filename
-                        funcname = frame.f_code.co_name
-                        lineno = frame.f_lineno
-                        stack.append(f"{funcname} ({filename}:{lineno})")
-                        frame = frame.f_back
-                    self._data[tuple(stack)] += 1
-            frame = None
-            stop_event.wait(self._period)
+                    sample(frame)
+            wait()
 
-        # Dump profiling data
-        with open("flamegraph.out", "w") as fp:
-            for stack, hits in self._data.items():
-                print(";".join(stack[::-1]), hits, file=fp)
-
+        self.sampler.dump()
         stop_event.clear()
         self.clean_exit = True
+
+    def __call__(self, func):
+        """
+        Allows to use `Profile` as a decorator:
+
+        @Profile(period=0.01, single=True)
+        def slow_function():
+            ...
+        """
+
+        @wraps(func)
+        def pfunc(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+
+        return pfunc
